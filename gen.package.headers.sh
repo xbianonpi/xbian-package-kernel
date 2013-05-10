cfacb4e8beb8f2343d44093eea6864b209dfa3d6
Matus Kral <matuskral@me.com>
2013-05-10 05:27:05 +0200

    fix

--- gen.package.headers.sh
+++ gen.package.headers.sh
@@ -3,5 +3,5 @@
 package=$(cat ./xbian-package-headers/DEBIAN/control | grep Package | awk '{print $2}')
 version=$(cat ./xbian-package-headers/DEBIAN/control | grep Version | awk '{print $2}')
 
-fakeroot find ./xbian-package-headers  | grep -v DEBIAN/ | xargs md5sum > ./xbian-package-headers/DEBIAN/md5sums 
+fakeroot find ./xbian-package-headers  | grep -v DEBIAN/ | xargs md5sum > ./xbian-package-headers/DEBIAN/md5sums > /dev/null 2>&1
 fakeroot dpkg-deb -b ./xbian-package-headers "${package}"-"${version}".deb
--- gen.package.image.sh
+++ gen.package.image.sh
@@ -3,5 +3,5 @@
 package=$(cat ./xbian-package-kernel/DEBIAN/control | grep Package | awk '{print $2}')
 version=$(cat ./xbian-package-kernel/DEBIAN/control | grep Version | awk '{print $2}')
 
-fakeroot find ./xbian-package-kernel  | grep -v DEBIAN/ | xargs md5sum > ./xbian-package-kernel/DEBIAN/md5sums 
+fakeroot find ./xbian-package-kernel  | grep -v DEBIAN/ | xargs md5sum > ./xbian-package-kernel/DEBIAN/md5sums > /dev/null 2>&1
 fakeroot dpkg-deb -b ./xbian-package-kernel "${package}"-"${version}".deb
