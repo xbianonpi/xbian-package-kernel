6d30337e0ff0d214ffd933940b2a4481ccdd5a25
Koenkk <koenkanters94@gmail.com>
2013-05-10 21:27:00 +0300

    Removed "-" between packagename and version

--- gen.package.image.sh
+++ gen.package.image.sh
@@ -4,4 +4,4 @@ package=$(cat ./xbian-package-kernel/DEBIAN/control | grep Package | awk '{print
 version=$(cat ./xbian-package-kernel/DEBIAN/control | grep Version | awk '{print $2}')
 
 fakeroot find ./xbian-package-kernel  | grep -v DEBIAN/ | xargs md5sum > ./xbian-package-kernel/DEBIAN/md5sums > /dev/null 2>&1
-fakeroot dpkg-deb -b ./xbian-package-kernel "${package}"-"${version}".deb
+fakeroot dpkg-deb -b ./xbian-package-kernel "${package}""${version}".deb
