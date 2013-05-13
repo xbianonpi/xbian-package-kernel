28ccc362de62b64c72bba332133a1ad0d38113f3
Matus Kral <matuskral@me.com>
2013-05-13 05:35:34 +0200

    moving to std dir

---
+++ gen.package.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+package=$(cat ./xbian-package-kernel/DEBIAN/control | grep Package | awk '{print $2}')
+version=$(cat ./xbian-package-kernel/DEBIAN/control | grep Version | awk '{print $2}')
+
+fakeroot find ./xbian-package-kernel  | grep -v DEBIAN/ | xargs md5sum > ./xbian-package-kernel/DEBIAN/md5sums > /dev/null 2>&1
+fakeroot dpkg-deb -b ./xbian-package-kernel "${package}""${version}".deb
