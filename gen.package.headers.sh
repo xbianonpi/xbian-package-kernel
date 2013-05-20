7d6c3c0c5f8d160ee62186201512b5614c574b73
Koenkk <koenkanters94@gmail.com>
2013-05-20 19:08:28 +0200

    Added 3.6.11 kernel

--- gen.package.headers.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-package=$(cat ./xbian-package-headers/DEBIAN/control | grep Package | awk '{print $2}')
-version=$(cat ./xbian-package-headers/DEBIAN/control | grep Version | awk '{print $2}')
-
-fakeroot find ./xbian-package-headers  | grep -v DEBIAN/ | xargs md5sum > ./xbian-package-headers/DEBIAN/md5sums > /dev/null 2>&1
-fakeroot dpkg-deb -b ./xbian-package-headers "${package}"-"${version}".deb
