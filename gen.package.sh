690dad82bd755faa728f8b16d1dd6707051ba643
Koenkk <koenkanters94@gmail.com>
2013-05-20 19:20:12 +0200

    Put a - between kernel version and package version

--- gen.package.sh
+++ gen.package.sh
@@ -8,4 +8,4 @@ rm ./content/lib/modules/*/source
 find ./content -print0 | xargs -0 -I{} sh -c ' test -d "{}" || echo $(cat "{}" | md5sum | ./fr.sh ) " {}" | grep -v DEBIAN/ | grep -v .DS_Store' > ./content/DEBIAN/md5sums
 cat ./content/DEBIAN/md5sums | sort -k2 > ./content/DEBIAN/md5sums.new
 mv ./content/DEBIAN/md5sums.new ./content/DEBIAN/md5sums
-fakeroot dpkg-deb -b ./content "${package}""${version}".deb
+fakeroot dpkg-deb -b ./content "${package}"-"${version}".deb
