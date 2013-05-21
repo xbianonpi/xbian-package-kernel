4dfbcabefe7e4f90328ce8250d7ed3610e17f3d4
Koenkk <koenkanters94@gmail.com>
2013-05-21 22:30:18 +0200

    Calculate package size

--- gen.package.sh
+++ gen.package.sh
@@ -3,6 +3,10 @@
 package=$(cat ./content/DEBIAN/control | grep Package | awk '{print $2}')
 version=$(cat ./content/DEBIAN/control | grep Version | awk '{print $2}')
 
+cat ./content/DEBIAN/control | grep -v "Installed-Size:" > ./content/DEBIAN/control.new
+mv ./content/DEBIAN/control.new ./content/DEBIAN/control
+printf "Installed-Size: %d\n" $(du -s ./content | awk '{print $1}') >> ./content/DEBIAN/control
+
 rm ./content/lib/modules/*/build
 rm ./content/lib/modules/*/source
 find ./content -print0 | xargs -0 -I{} sh -c ' test -d "{}" || echo $(cat "{}" | md5sum | ./fr.sh ) " {}" | grep -v DEBIAN/ | grep -v .DS_Store' > ./content/DEBIAN/md5sums
